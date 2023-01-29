fe81617f9bc6c01deb5ec6f27c0fdf7f5acd46c8
wakeupChen <123854904+wakeupChen@users.noreply.github.com>
2023-01-29 14:06:59 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Interview with vitePress',
     // 打包目录
     dest: './dist',
-    base:'/interview/',
+    base:'/chenBlog/',
     // 头部head
     head: [
         // 添加图标
